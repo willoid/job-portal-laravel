034e25900a3851421bac63751f04178d599c5a8a
willy <willoid.webdev@gmail.com>
2025-10-29 21:27:23 +0100

    added favicon

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,16 +1,13 @@
 {{-- resources/views/components/application-logo.blade.php --}}
-<svg xmlns="http://www.w3.org/2000/svg" class="logo-svg" aria-hidden="true" focusable="false" viewBox="0 0 885 748" shape-rendering="crispEdges">
-    <path d="M 876,10 L 687,8 L 452,9 L 351,79 L 352,566 L 349,569 L 223,569 L 221,567 L 222,473 L 220,464 L 95,463 L 8,524 L 10,737 L 13,739 L 446,739 L 451,737 L 516,676 L 527,663 L 527,180 L 529,178 L 706,180 L 706,226 L 704,228 L 633,228 L 568,272 L 558,276 L 557,369 L 560,380 L 789,380 L 875,314 Z" fill="#000" stroke="1"/>
-    <path d="M 18,526 L 25,519 L 32,519 L 98,474 L 197,474 L 211,471 L 214,474 L 214,478 L 211,480 L 211,571 L 158,609 L 144,623 L 151,625 L 157,623 L 218,579 L 348,578 L 351,580 L 352,626 L 156,628 L 149,632 L 144,632 L 139,628 L 139,624 L 142,623 L 142,531 L 209,483 L 209,480 L 206,480 L 204,477 L 180,490 L 135,522 L 31,523 L 30,526 L 22,528 Z" fill="#fff" stroke="none"/>
-    <path d="M 575,277 L 594,267 L 637,238 L 706,239 L 707,273 L 705,276 L 589,276 L 577,279 Z" fill="#fff" stroke="1"/>
-    <path d="M 514,178 L 517,180 L 516,660 L 452,721 L 450,719 L 452,617 L 451,181 L 454,178 Z" fill="#fff" stroke="none"/>
-    <path d="M 781,87 L 782,362 L 785,368 L 783,370 L 569,369 L 567,366 L 567,288 L 569,286 L 716,285 L 716,168 L 442,168 L 440,170 L 440,705 L 442,724 L 440,728 L 21,728 L 19,726 L 20,583 L 18,535 L 20,532 L 131,533 L 131,615 L 133,638 L 360,637 L 362,635 L 362,88 Z" fill="#fff" stroke="none"/>
-    <path d="M 864,28 L 865,308 L 794,364 L 792,362 L 792,86 Z" fill="#fff" stroke="none"/>
-    <path d="M 868,20 L 865,26 L 855,23 L 844,30 L 787,76 L 374,77 L 456,19 L 851,19 L 864,16 Z" fill="#fff" stroke="1"/>
+<svg xmlns="http://www.w3.org/2000/svg" class="logo-svg" aria-hidden="true" focusable="false" viewBox="0 0 32 32" shape-rendering="crispEdges">
+    <path d="M11 4a2 2 0 012-2h6a2 2 0 012 2v6h-2V4h-6v6h-2V4z" fill="#1e293b"/>
+   <rect x="4" y="10" width="24" height="18" rx="2" fill="#3b82f6"/>
+    <rect x="14" y="16" width="4" height="3" rx="0.5" fill="#fbbf24"/>
 </svg>
 <style>
     .logo-svg{
-        width: 90px;
+        width: 100%;
+        max-width: 50px;
         height: auto;
     }
 </style>
